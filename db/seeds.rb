c0c7c9e0fd64cf3ef59e4cbd9bac422165869921
yui <yuikita21@gmail.com>
2018-10-29 19:57:15 +0900

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Page.create(title: 'Cesar Sayoc, Mail Bombing Suspect', content: 'AVENTURA, Fla. — Cesar Sayoc Jr. was a volatile nobody desperate to become a somebody.')
 
-Page.create({'title': 'シン・ゴジラみてきた', 'content': 'シンゴジラみてきた。人間が作った技術で出来上がった生物を人間が技術で倒す映画。ゴジラは人間の技術の化身。\\n\nゴジラが米軍の貫通弾を食らって苦しそうに反撃していた。絶叫して東京を火の海にした。放射能の海にした。ゴジラは虚構ではない。\n\n鎌倉とか多摩川とかいつも暮らしている町が出てきて、すごくリアリティがあった。頭ではゴジラは倒されるとわかっていても、ゴジラ頑張れってなった。東京なんて焼き払っちゃえばいいと思った。\n\n私たちは技術や社会の上で生きている。そこには見えない犠牲がたくさんある。ゴジラは犠牲者の化身だ。言葉にならない憎しみを叫ぶ幼い生物だ。\n\nゴジラがんばれ！'})
